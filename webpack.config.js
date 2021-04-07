e2e9683be7a8835f795086dc8a288bcf49706644
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-04-07 11:50:09 -0700

    Box 1: Added all necessary dependencies and deveDependencies with directories

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: clientPath,
+  output: {
+    path: serverPublicPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: clientPath,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: process.env.DEV_SERVER_PORT,
+    publicPath: '/',
+    contentBase: serverPublicPath,
+    watchContentBase: true,
+    stats: 'minimal',
+    proxy: {
+      '/api': `http://localhost:${process.env.PORT}`
+    }
+  },
+  performance: {
+    hints: false
+  }
+};
