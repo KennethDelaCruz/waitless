dba2f8cde63579a8abfc1175d984c0ad821e57c5
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-04-09 20:44:37 -0700

    refactored the RestaurantList function to become a class that has a state, added the feature to change the state depending on which restaurant was clicked

--- client/components/restaurant-list.jsx
+++ client/components/restaurant-list.jsx
@@ -29,23 +29,57 @@ function Restaurant(props) {
     boxShadow: '2px 2px 2px rgb(110, 110, 110)'
   };
   return (
-    <div className="column-half restaurant-card">
+    <>
       <div className="image-container" style={style}/>
-      <div className="description" style={{ border: '2px solid black' }}>
-        <h4>{props.info.name}</h4>
+      <div className="description" >
+        <h4 style={{ marginBottom: 0 }}>{props.info.name}</h4>
+        <p style={{ margin: 0 }}>
+          {`rating placeholder ${props.info.rating}`}
+          <span style={{ fontSize: '10px', marginLeft: '4px' }}>
+            {`${props.info.review_count} reviews`}
+          </span>
+        </p>
+        <p style={{ margin: 0, fontSize: '11px' }}>
+          {props.info.categories.map(i => { return `${i.title}, `; })}
+        </p>
       </div>
-    </div>
+      <div className={props.selected}>
+        <p></p>
+      </div>
+    </>
   );
 }
 
-function RestaurantList(props) {
-  return (
-    <div className="container restaurant-list">
-      {props.restaurants.map((restaurant, i) => {
-        return <Restaurant key={i} info={restaurant} />;
-      })}
-    </div>
-  );
+class RestaurantList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: null };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(i) {
+    if (this.state.selected === i) {
+      this.setState({ selected: null });
+    } else {
+      this.setState({ selected: i });
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="container restaurant-list">
+        {this.props.restaurants.map((restaurant, i) => {
+          return (
+            <div key={i} onClick={() => this.handleClick(i)} className="column-half restaurant-card">
+              <Restaurant onClick={i => this.handleClick(i)} info={restaurant} />;
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
 }
 
 export default RestaurantList;
